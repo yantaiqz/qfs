9c2c8f5dd290c17a53545b0453edb33c400e6126
yantaiqz <yantaiqz@googlemail.com>
2025-12-12 22:08:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -312,7 +312,7 @@ def initialize_model():
     model = genai.GenerativeModel(
         # model_name='gemini-2.5-flash', 
         #  model_name='gemini-2.5-pro', 
-        model_name='gemini-2.0-flash', 
+        model_name='gemini-1.5-pro', 
         system_instruction=SYSTEM_INSTRUCTION,
         generation_config=generation_config
     )
